efc0893efaed21392bb843c2503fa62f379efa34
Felipe-Clink-Teixeira <fcteixeira200@gmail.com>
2023-10-30 21:35:51 -0300

    primeiro commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    
+    
+        $("#formulario").submit(function(e)
+        
+           
+           {
+             e.preventDefault();
+             Cadastrar();
+           }
+        );
+    }
+);
+
+function Cadastrar()
+{
+   let variaveis = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val(),
+    };
+
+    $.post("/Home/Cadastrar", variaveis)
+
+    .done(function(data)
+        {
+            if(data.status == "OK")
+            {
+                $("#formulario").after("<h3>Cadastro realizado com sucesso</h3>");
+                $("#formulario").hide();
+            }
+            else
+            {
+               alert(data.mensagem);
+            }
+        }
+    )
+    .fail(function()
+        {
+            alert("Erro!!");
+        }
+
+    )
+};
